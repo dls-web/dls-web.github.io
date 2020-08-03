c5589b04a096c2c08c63715e01a9383157ddba0f
Dmitrij Starcenko <dlstarcenko@gmail.com>
2020-08-03 13:55:43 +0100

    Update

---
+++ precache-manifest.6d5e1dcf30ff6bc5dd3c768989b6b1d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fe071a2a5cb32254566d945427b018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6ddb0dbbb751aa09d8",
+    "url": "/static/css/main.29dbda2d.chunk.css"
+  },
+  {
+    "revision": "208f57a1987a2b0ed14c",
+    "url": "/static/js/2.147ce43c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.147ce43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6ddb0dbbb751aa09d8",
+    "url": "/static/js/main.c3f91b09.chunk.js"
+  },
+  {
+    "revision": "2a8d64df1838342fafc1",
+    "url": "/static/js/runtime-main.47379d69.js"
+  },
+  {
+    "revision": "2786db22b04ddcde89025af20b0f11c7",
+    "url": "/static/media/me.2786db22.png"
+  },
+  {
+    "revision": "e4f1dbd6494cc4b202bbb229365c9915",
+    "url": "/static/media/niko_img.e4f1dbd6.jpg"
+  }
+]);
